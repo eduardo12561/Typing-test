d261921a43abeaeb6a5fcf0f976e72c6d404c80f
Eduardo Breda <pontes.cisa@gmail.com>
2022-03-10 10:28:05 -0300

    finish the funtionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const typingText = document.querySelector(".typing-text p");
 const inpField = document.querySelector(".wrapper .input-field");
+const mistakeTag = document.querySelector(".mistake span");
+const timeTag = document.querySelector(".time span b");
+const wpmTag  = document.querySelector(".wpm span");
+const cpmTag  = document.querySelector(".cpm span");
+const tryAgainBtn = document.querySelector("button");
 
-let charIndex = 0;
+let timer,
+maxTime = 60,
+timeLeft = maxTime,
+charIndex = mistakes = isTyping = 0;
 
 function randomParagraph () {
     let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
     paragraphs[randIndex].split("").forEach(span => {
         let spanTag = `<span>${span}</span>`;
         typingText.innerHTML += spanTag
     });
+    typingText.querySelectorAll("span")[0].classList.add("active");
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
 }
@@ -16,19 +26,58 @@ function randomParagraph () {
 function initTyping() {
     const chracters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
-    if(typedChar == null) {
-        charIndex--;
-        chracters[charIndex].classList.remove("correct", "incorrect")
-    } else {
-        if(chracters[charIndex].innerText === typedChar) {
-            chracters[charIndex].classList.add("correct");
-        } else {
-            chracters[charIndex].classList.add("incorrect");
+    if(charIndex < chracters.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
         }
-        charIndex++;;
+        if(typedChar == null) {
+            charIndex--;
+            if(chracters[charIndex].classList.contains("incorrect")) {
+                mistakes--;
+            }
+            chracters[charIndex].classList.remove("correct", "incorrect")
+        } else {
+            if(chracters[charIndex].innerText === typedChar) {
+                chracters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                chracters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;;
+        }
+        let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        mistakeTag.innerText = mistakes;
+        wpmTag.innerText = wpm;
+        cpmTag.innerText = charIndex -mistakes;    
+    } else {
+        inpField.value = "";
+        clearInterval(timer)
     }
-    
+}
+
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+    } else {
+        clearInterval(timer)
+    }
+}
+
+function resetGame() {
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer)
+    timeLeft = maxTime,
+    charIndex = mistakes = isTyping = 0;
+    timeTag.innerText = timeLeft;
+    mistakeTag.innerText = mistakes;
+    wpmTag.innerText = 0;
+    cpmTag.innerText = 0;  
 }
 
 randomParagraph();
 inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
